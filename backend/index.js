179da3b88a88ea1cb27a645d8455a8343bdfa448
Mugilvannan P <mugilvannanpichai02@gmail.com>
2025-01-06 10:53:58 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
-const corsOption={
-    origin:'https://mern-chat-app-frontend-0wea.onrender.com',
-    credentials:true
+const corsOption = {
+    origin: '*',
+    credentials: true
 };
 app.use(cors(corsOption)); 
 
